703c4ba68e125fa927d1d0f41175c56d0427ca09
Your Name <you@example.com>
2022-12-12 19:55:14 +0300

    alphabeat soup
    prehistoric sounds
    
    sandstone
    
    sand
    
    buckwheat
    
    cauliflower
    
    asparagus
    
    Big black oily monkey
    
    markiplier
    
    big chungus
    
    a drawing of a person sitting on a horse, a child's drawing, by Pablo Picasso, net art, plumbus, half man half bear half pig, path based unbiased rendering, roblox avatar, bald patch, hairy orange skin, he has an elongated head shape, fractal flame. highly_detailded
    
    ahahaha
    DAMN
    I AM BRIAN
    idiot idiididididididiiot remoemoermoermerve the CASP LOCK 123456
    CASOP LSALSALSALSAL
    MD....
    
    innit doe bruv innit tbh kinda innit doe innit lowkey kinda innit smh doe bruv innit kinda lowkey tbh bruv innit doe kinda smh bruv innit doe lowkey smh doe innit innit doe tbh lowkey doe lowkey innit kinda bruv smh doe doe smh lowkey innit bruv doe smh tbh doe innit
    
    man cow it idk sine lol mipe car pm damn sort wax end lips pork dip sure and cone alt pee none salt pain care hip dome lad gg soda wip case roar god lame sure gip oof ban sit cope lick dupe felt can
    
    is it allowed to advertise discord servers such as big chungus server or the amongus server?????
    
    a
    
    whadoseitdo doe
    
    pls tell
    what a this
    
    aight good to know btw lol idk hahah bababannanaanamamaa,a,a,a
    
    idkman
    
    alrightman
    
    123456
    
    sadsfdghfdsasfdgsfadsd
    
    Where is the Forge version so people can actually use this?
    
    port to forge so that actual people can use this as well

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ def BeatSwap(pattern: str):
     song.quick_beatswap(output=None, pattern=pattern)
     return song.audio
 
-ui=gr.Interface (fn=BeatSwap,inputs="audio",outputs="audio" )
+ui=gr.Interface (fn=BeatSwap,inputs="audio",outputs="audio",theme="default" )
 ui.launch
