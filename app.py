929822efb95c5366c6018b02191bed7ef653cca1
Ivan Nikishev <dpe1@users.noreply.huggingface.co>
2023-11-24 19:49:21 +0000

    gradio fix

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+os.system("pip uninstall -y gradio")
+os.system("pip install gradio==2.6.4")
 import gradio as gr, numpy as np
 from gradio.components import Audio, Textbox, Checkbox, Image
 import beat_manipulator as bm
